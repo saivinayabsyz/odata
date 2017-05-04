bceeffcac5895382581ee72eac3f96fcc8df7f08
spring-work <chintanjshah@gmail.com>
2017-05-04 08:17:54 -0700

    test

--- src/main/java/com/spring/heroku/odata/servlet/InfoServlet.java
+++ src/main/java/com/spring/heroku/odata/servlet/InfoServlet.java
@@ -26,6 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 )
 public class InfoServlet extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException {
         try {
